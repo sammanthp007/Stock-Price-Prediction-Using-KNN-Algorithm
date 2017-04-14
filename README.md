a262b3cd32774109d507cde6fca938ebe9c55561
sammanthp007 <samman.thapa@bison.howard.edu>
2017-04-14 14:42:10 -0400

    import from another file

--- README.md
+++ README.md
@@ -12,3 +12,33 @@ pip install -r requirements.txt
 
 python predictStockRiseOrFall.py
 ```
+
+**OPEN** is the proce of the stock at tha beginning of the trading day (it 
+need not be the closing price of the previous day).
+
+**High** is the highest price of the stock at closing time.
+
+**Low** is the lowest price of the stock on that trading day.
+
+**Close** is the price of the stock at closing time.
+
+**Volume** indicates how many stocks were traded.
+
+**Adjusted Close** is the closing price of the stock that adjusts the price of 
+the stock for corporate actions.
+
+While stock prices are considered to be set mostly by traders, **stock** splits
+(when the company makes each extant stock worth two and halves the price) and
+**dividends** (payout of company profits per share) also affect the price of a 
+stock and should be accounted for.
+
+### Visualizing data using matplotlib
+A linechart is fine, but we are considering 5 data points as important data.
+They include Trading Volume, Open, Highest, Lowest, and Closing prices. So, we
+would like to visualize these data in a way that all five variables can be shown
+in the same graph, and not in 5 separate graphs. *Financial data is ofter 
+plotted with a* **Japanese candlestick plot**. We can create such diagram in
+matplotlib, but we have implemented the diagram in `plotting_functions.py`.
+
+
+
--- main.py
+++ main.py
@@ -7,38 +7,28 @@
 # print(mydata)
 
 import pandas as pd
-# import pandas.io.data as web   # Package and modules for importing data; this code may change depending on pandas version
-from pandas_datareader import data, wb
 import pandas_datareader as pdr
+import pandas_datareader.data as web
 import datetime
- 
-# We will look at stock prices over the past year, starting at January 1, 2016
-start = datetime.datetime(2016,1,1)
-end = datetime.date.today()
- 
-# Let's get Apple stock data; Apple's ticker symbol is AAPL
-# First argument is the series we want, second is the source ("yahoo" for Yahoo! Finance), third is the start date, fourth is the end date
-# apple = wb.DataReader("AAPL", "yahoo", start, end)
-apple = pdr.get_data_yahoo('AAPL')
- 
-print (type(apple))
-print(apple.head())
-
-
-f = pdr.DataReader("F", "yahoo", start, end)
 
 import matplotlib.pyplot as plt   # Import matplotlib
-# This line is necessary for the plot to appear in a Jupyter notebook
 
-# Control the default size of figures in this Jupyter notebook
+from plottingfuncs import sayHello
 
-# pylab.rcParams['figure.figsize'] = (15, 9)   # Change the size of plots
-# plt.plot([1,2,3,4])
-# plt.show()
-apple["Adj Close"].plot(grid = True) # Plot the adjusted closing price of AAPL
+# We will look at stock prices over the past year, starting at January 1, 2016
+start = datetime.datetime(2007,1,1)
+end = datetime.date.today()
+
+# Let's get Apple stock data; Apple's ticker symbol is AAPL
+apple = web.DataReader('AAPL', 'yahoo', start, end)
+
+print(apple.head())
+
+# apple["Adj Close"].plot(grid = True) # Plot the adjusted closing price of AAPL
 print(apple["Adj Close"])
 apl = apple["Adj Close"]
 
+sayHello()
 
 plt.plot(apl)
 plt.show()
