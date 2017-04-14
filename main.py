1a152b37ed08c48f0deb6943d30ad716e1fd54bd
sammanthp007 <samman.thapa@bison.howard.edu>
2017-04-14 16:45:27 -0400

    graph with different y-axis done

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import pandas_datareader as pdr
 import pandas_datareader.data as web
 import datetime
 
+import pandas as pd
+
 import matplotlib.pyplot as plt   # Import matplotlib
 
 import plotly.plotly as py
@@ -23,16 +25,27 @@ end = datetime.date.today()
 
 # Let's get Apple stock data; Apple's ticker symbol is AAPL
 apple = web.DataReader('AAPL', 'yahoo', start, end)
+microsoft = web.DataReader("MSFT", "yahoo", start, end)
+disney = web.DataReader("DIS", "yahoo", start, end)
+nugt = web.DataReader("NUGT", "yahoo", start, end)
+dust = web.DataReader("DUST", "yahoo", start, end)
 
-print(apple.head())
+# Below I create a DataFrame consisting of the adjusted closing price of these stocks, first by making a list of these objects and using the join method
+stocks = pd.DataFrame({"AAPL": apple["Adj Close"],
+                      "DIS": disney["Adj Close"],
+                      "NUGT": nugt["Adj Close"],
+                      "DUST": dust["Adj Close"]})
 
-# apple["Adj Close"].plot(grid = True) # Plot the adjusted closing price of AAPL
-print(apple["Adj Close"])
-apl = apple["Adj Close"]
+print(stocks.head())
+
+ax1 = stocks.plot(secondary_y = ["AAPL", "DIS"],grid=True)
+lines, labels = ax1.get_legend_handles_labels()
+
+ax1.legend(lines[:5], labels[:5], loc="best")
 
 
-plt.plot(apl)
-# plt.show()
+# plt.plot(stocks)
+plt.show()
 
 df = web.DataReader('AAPL', 'yahoo', start, end)
 trace = go.Candlestick(x=df.index,
@@ -42,4 +55,6 @@ trace = go.Candlestick(x=df.index,
                        close=df.Close
                        )
 data = [trace]
-plotly.offline.plot(data)
+# plotly.offline.plot(data)
+
+
