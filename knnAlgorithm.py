c2345de32a997b92f94f2be742bda8358e6ed68f
sammanthp007 <samman.thapa@bison.howard.edu>
2017-04-15 22:57:57 -0400

    add date to the input too

--- knnAlgorithm.py
+++ knnAlgorithm.py
@@ -6,13 +6,13 @@ import operator
 import pandas_datareader.data as web
 import datetime
 
+import matplotlib.pyplot as plt   # Import matplotlib
+import json
+
+forDate = []
 # split the data into a trainingdataset and testdataset in ratio of 67/33
+
 def loadDataset(filename, split, trainingSet=[], testSet=[], content_header=[]):
-    # open the file
-    # data format:
-    # sepal length, sepal width, petal length, petal width, classification
-    # e.g 
-    # 5.9, 3.0, 5.1, 1.8, Iris-virginica
     with open(filename, 'rb') as csvfile:
         # returns a reader object which will iterate over lines
         lines = csv.reader(csvfile)
@@ -22,17 +22,18 @@ def loadDataset(filename, split, trainingSet=[], testSet=[], content_header=[]):
         for x in range(len(dataset) - 1):
             # convert the content to float
             # minus 1 because last is string for up or down
-            for y in range(len(content_header) - 1):
+            for y in range(1, len(content_header) - 1):
                 dataset[x][y] = float(dataset[x][y])
             if random.random() < split:
                 trainingSet.append(dataset[x])
             else:
                 testSet.append(dataset[x])
+               #  print(dataset[x])
 
 
 def euclideanDistance(instance1, instance2, length):
     distance = 0
-    for x in range(length):
+    for x in range(1, length):
         distance += pow((instance1[x] - instance2[x]), 2)
     return math.sqrt(distance)
 
@@ -84,41 +85,30 @@ def getAccuracy1(testSet, predictions):
             correct += 1
     return (correct/float(len(testSet))) * 100.0
 
+def drawGraph(testSet, predictions):
+    toFloatPredictions = []
+    toFloatTestSet = []
+    for i in predictions:
+        if i == "up":
+            toFloatPredictions.append(1)
+        else:
+            toFloatPredictions.append(-1)
+
+    for x in range(len(testSet)):
+        if testSet[x][-1] == "up":
+            toFloatTestSet.append(1)
+        else:
+            toFloatTestSet.append(-1)
+    # print(toFloatTestSet[0], len(toFloatTestSet), toFloatTestSet)
+    # print(toFloatPredictions[0], len(toFloatPredictions))
+    plt.plot(toFloatTestSet, toFloatPredictions)
+    # plt.show()
+
+
+
 
 def RMSD(X, Y):
     return math.sqrt(pow(Y - X, 2))
-
-
-def main():
-    # iv = ["sepal length", "sepal width", "petal length", "petal width"]
-    iv = ["open", "high", "low", "closing adj", "state change"]
-    trainingSet = []
-    testSet = []
-    # changable values
-    split = 0.67
-    # number of neighbors
-    k = 5
-    # name of file to get and set data to
-    filename = 'apple.csv'
-    loadDataset(filename, split, trainingSet, testSet, iv)
-
-    print("Train: " + repr(len(trainingSet)))
-    print("Test: " + repr(len(testSet)))
-
-    # generate predictions
-    predictions = []
-    for x in range(len(testSet)):
-        neighbors = getNeighbors(trainingSet, testSet[x], k)
-        result = getResponse(neighbors)
-        predictions.append(result)
-        # print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
-
-    accuracy = getAccuracy(testSet, predictions)
-
-# [5.1, 3.5, 1.4, 0.2, 'Iris-setosa']
-# [4.7, 3.2, 1.3, 0.2, 'Iris-setosa']
-
-    print('Accuracy: ' + repr(accuracy) + '%')
 
 
 def getData1(filename, startdate, enddate):
@@ -127,22 +117,103 @@ def getData1(filename, startdate, enddate):
     with open('apple.csv', 'wb') as csvfile:
         stockwriter = csv.writer(csvfile, quotechar=',')
         for ind in range(len(apple.Open)):
-            stockwriter.writerow([apple.High[ind]] + [apple.Low[ind]] + [apple['Adj Close'][ind]])
+            stockwriter.writerow([apple.High[ind]] + [apple.Low[ind]] +
+                    [apple['Adj Close'][ind]])
+
 
 def change(today, yest):
-    if today >= yest:
+    if today > yest:
         return 'up'
     return 'down'
 
-def getData(filename, startdate, enddate):
-    apple = web.DataReader('AAPL', 'yahoo', startdate, enddate)
-    with open('apple.csv', 'wb') as csvfile:
+
+def getData(filename, stockname, startdate, enddate):
+    apple = web.DataReader(stockname, 'yahoo', startdate, enddate)
+    print("done making network call")
+    ass = [apple.index]
+    stck_json = apple.to_json(orient="index", date_format='iso')
+    stck_dates = json.loads(stck_json)
+
+    first_time = True
+    with open(filename, 'wb') as pp:
+        stockwriter = csv.writer(pp)
+        for i in stck_dates.keys():
+            new_format_date = i[:10]
+            if first_time:
+                first_time = False
+                prev_closing = stck_dates[i]["Adj Close"]
+                continue
+            # print(new_format_date)
+            # p = datetime.datetime.strptime(new_format_date, '%Y-%M-%d')
+            # print(p)
+            stockwriter.writerow([new_format_date] + [stck_dates[i]["Open"]] +  [stck_dates[i]["High"]] + [stck_dates[i]["Low"]]  +  [stck_dates[i]["Adj Close"]] + [change(stck_dates[i]["Adj Close"], prev_closing)])
+            prev_closing = stck_dates[i]["Adj Close"]
+
+#     with open(filename, 'wb') as csvfile:
+#         stockwriter = csv.writer(csvfile, quotechar=',')
+#         for ind in range(1, len(apple.Open)):
+#             stockwriter.writerow([apple.Open[ind - 1]] + [apple.High[ind - 1]]
+#                     + [apple.Low[ind - 1]] + [apple['Adj Close'][ind - 1]] +
+#                     [change(apple['Adj Close'][ind], apple['Adj Close'][ind -
+#                         1])])
+
+def abc(filename, stockname, startdate, enddate):
+    apple = web.DataReader(stockname, 'yahoo', startdate, enddate)
+    with open(filename, 'wb') as csvfile:
         stockwriter = csv.writer(csvfile, quotechar=',')
         for ind in range(1, len(apple.Open)):
-            stockwriter.writerow([apple.Open[ind - 1]] + [apple.High[ind - 1]] + [apple.Low[ind - 1]] + [apple['Adj Close'][ind - 1]] + [change(apple['Adj Close'][ind], apple['Adj Close'][ind - 1])])
+            stockwriter.writerow(["open: "] + [apple.Open[ind - 1]] + ["    high: "] + [apple.High[ind - 1]] + ["   low: "] + [apple.Low[ind - 1]] + ["  yester close: "] + [apple['Adj Close'][ind - 1]] + [" volume: "] + [apple.Volume[ind - 1]] + [change(apple['Adj Close'][ind], apple['Adj Close'][ind - 1])])
 
-start = datetime.datetime(2011,1,1)
-enddate = datetime.date.today()
 
-getData('apple.csv', start, enddate)
+def main():
+    split = 0.67
+    # set data
+    startdate = datetime.datetime(2001,1,1)
+    enddate = datetime.date.today()
+
+    # predictFor(5, 'dust.csv', 'DUST', startdate, enddate, 0, split)
+    # predictFor(5, 'apple.csv', 'AAPL', startdate, enddate, 0, split)
+    # predictFor(5, 'disney.csv', 'DIS', startdate, enddate, 0, split)
+    # predictFor(5, 'yahoo.csv', 'YHOO', startdate, enddate, 0, split)
+    # predictFor(5, 'nugt.csv', 'NUGT', startdate, enddate, 0, split)
+    predictFor(5, 'twlo.csv', 'TWLO', startdate, enddate, 1, split)
+    # predictFor(5, 'twtr.csv', 'TWTR', startdate, enddate, 1, split)
+
+
+def predictFor(k, filename, stockname, startdate, enddate, writeAgain, split):
+    iv = ["date", "open", "high", "low", "yesterday closing adj", "state change"]
+    trainingSet = []
+    testSet = []
+
+    # abc('abc.csv', stockname, start, enddate)
+    if writeAgain:
+        print("making a network request")
+        getData(filename, stockname, startdate, enddate)
+        print("done making a network request")
+
+    # open the file
+    loadDataset(filename, split, trainingSet, testSet, iv)
+
+    print("Predicting for ", stockname)
+    print("Train: " + repr(len(trainingSet)))
+    print("Test: " + repr(len(testSet)))
+
+    # generate predictions
+    predict_and_get_accuracy(testSet, trainingSet, k)
+
+def predict_and_get_accuracy(testSet, trainingSet, k):
+    predictions = []
+    for x in range(len(testSet)):
+        neighbors = getNeighbors(trainingSet, testSet[x], k)
+        result = getResponse(neighbors)
+        predictions.append(result)
+        # print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
+
+    accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: ' + repr(accuracy) + '%')
+
+    drawGraph(testSet, predictions)
+
+
+
 main()
